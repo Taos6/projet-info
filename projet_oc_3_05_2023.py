a845b6155511280e12e2ae574a7eaefc92422399
ABDUL SUBAHAN MUKHTAR AHMED <MUKHTAR.ABDLS@ge-pedago.etat-ge.ch>
2023-05-10 11:38:36 +0200

    fin creation de la balle

---
+++ projet_oc_3_05_2023.py
@@ -0,0 +1,15 @@
+import turtle as tr
+
+window = tr.Screen()
+window.bgcolor("black")
+window.setup(width=600, height=600)
+
+balle=tr.Turtle()
+balle.speed(0)
+balle.shape("circle")
+balle.color("yellow")
+balle.penup()
+balle.setposition(0,-200)
+
+
+
