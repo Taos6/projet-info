575cb625624ee1b2d707de87b66d8319005b39a2
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-13 15:01:11 +0200

    casse_brique_main
    ajout de la balle dans le programme principal

--- casse_brique_principal.py
+++ casse_brique_principal.py
@@ -13,6 +13,16 @@ barre.color("red")
 barre.penup()
 barre.setposition(0,-225)
 
+ball = tr.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.shapesize(stretch_wid = 1.5, stretch_len = 1.5)
+ball.color("red")
+ball.penup()
+ball.setposition(0,-150)
+ball.dx = -2
+ball.dy = -2
+
 def aller_droite():
     print("test")
     barre.forward(30)
