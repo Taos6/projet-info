c9a3597b4db65fccd968bd537eab55f9f428e4c5
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-13 16:20:49 +0200

    ajout deplacement_balle
    copie de la fonction deplacement_balle

--- casse_brique_principal.py
+++ casse_brique_principal.py
@@ -27,11 +27,28 @@ def aller_droite():
     print("test")
     barre.forward(30)
     
+    
+    
 def aller_gauche():
     barre.backward(30)
- 
-print("test")
- 
+
+
+
+def deplacement_balle():
+    while True:
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+        
+        if ball.xcor() > 280:     # Rebond à droite
+            ball.dx *= -1
+        elif ball.xcor() < -280:  # Rebond à gauche
+            ball.dx *= -1
+        elif ball.ycor() > 280:   # Rebond en haut
+            ball.dy *= -1
+        elif ball.ycor() < -280:  # Rebond en bas
+            ball.dy *= -1
+
+deplacement_balle()
 tr.onkey(aller_droite,"d")
 tr.onkey(aller_gauche,"a")
 window.listen()
