df5915a35746f7ca3b38051429c7d4cda5a8e11f
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-15 16:31:33 +0200

    création du fichier pour la barre

---
+++ creation_mouvement_barre.py
@@ -0,0 +1,37 @@
+"""Création et déplacement de la barre"""
+
+import turtle as tr
+
+window = tr.Screen()
+window.bgcolor("black")
+window.setup(width=600, height=600)
+
+
+barre = tr.Turtle()
+barre.speed(0)
+barre.shape("square")
+barre.shapesize(stretch_wid = 1, stretch_len=5)
+barre.color("red")
+barre.penup()
+barre.setposition(0,-225)
+
+
+def aller_droite():
+    """Permer à la barre d'aller à droite"""
+    x = barre.xcor()
+    x += 20
+    barre.setx(x)
+
+window.listen()
+window.onkeypress(aller_droite,"d")
+
+
+    
+def aller_gauche():
+    """Permet à la barre d'aller à gauche"""
+    x = barre.xcor()
+    x -= 20
+    barre.setx(x)
+
+window.listen()
+window.onkeypress(aller_gauche,"a")
