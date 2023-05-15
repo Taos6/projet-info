cdb8ec156b16bbe26a7e57f34e22a97ac502f8f5
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-15 16:06:42 +0200

    fichier gérant le game over

---
+++ game_over.py
@@ -0,0 +1,20 @@
+"""Gère la défaite"""
+
+
+def game_over_text():
+    """Fais apparaître le texte Game Over"""
+    game_over_text = tr.Turtle()
+    game_over_text.color("red")
+    game_over_text.penup()
+    game_over_text.goto(0, 50) # pour afficher le message plus haut  
+    game_over_text.write("Game Over :/", align = "center", font = ("chalkduster", 40, "normal"))
+    game_over_text.hideturtle()
+    
+
+def game_over():
+    """Stoppe la partie"""
+    ball.dx = 0
+    ball.dy = 0
+    ball.clear()
+    game_over_text()
+    text_restart_game()
