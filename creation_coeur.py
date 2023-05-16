9a2756b1971a39c6924bbd5c851e70d5b98420eb
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-16 18:49:23 +0200

    changement de nom des fichiers

--- creation_coeur.py
+++ creation_coeur.py
@@ -1,16 +1,40 @@
 import turtle as tr
-import game_over as over
-import creation_mouvement_balle as move_balle
-import creation_mouvement_barre as move_barre
+import time
 
-window = tr.Screen()
-window.bgcolor("black")
-window.setup(width=600, height=600)
+vie = 3
+tr.register_shape("image coeur.gif")
+
+carre1 = tr.Turtle()
+carre1.speed(0)
+carre1.shape("image coeur.gif")
+carre1.color("yellow")
+carre1.penup()
+carre1.setposition(280, 260)
+
+carre2 = tr.Turtle()
+carre2.speed(0)
+carre2.shape("image coeur.gif")
+carre2.color("yellow")
+carre2.penup()
+carre2.setposition(250, 260)
+
+carre3 = tr.Turtle()
+carre3.shape("image coeur.gif")
+carre3.speed(0)
+carre3.color("yellow")
+carre3.penup()
+carre3.setposition(220, 260)
 
 
-while True:
-    window.update()
-    move_balle.deplacement_balle()
+def nb_vie():
+    vies_text = tr.Turtle()
+    vies_text.speed(0)
+    vies_text.color("white")
+    vies_text.penup()
+    vies_text.setposition(280, 280)
+    vies_text.write("Vous avez 3 vies et les carrés représentent le nombre de vies restants", align="right", font=("Goudy Old Style", 15, "normal"))
+    vies_text.hideturtle()
     
-    if ball.ycor() < -280:
-         game_over()
+
+   
+   
