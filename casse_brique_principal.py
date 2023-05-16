79f674b5394fe4995d4cbcfbf34171e08043ef85
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-16 23:02:05 +0200

    importation du module collision_barre_balle

--- casse_brique_principal.py
+++ casse_brique_principal.py
@@ -3,6 +3,7 @@ import game_over as over
 import creation_mouvement_balle as move_balle
 import creation_mouvement_barre as move_barre
 import creation_coeur as coeur
+import collision_balle_barre as collision1
 
 window = tr.Screen()
 window.bgcolor("black")
@@ -13,6 +14,7 @@ coeur.nb_vie()
 while True:
     window.update()
     move_balle.deplacement_balle()
+    collision1.collision_barre()
     if move_balle.ball.ycor() < -280:
         print(move_balle.ball.ycor())
         coeur.vie -= 1
