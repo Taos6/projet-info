56ddc581216c88c9900c318743b59b9ff22923f5
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-17 02:55:48 +0200

    changement de certaines fonctions

--- partie_gagnée.py
+++ partie_gagnée.py
@@ -3,17 +3,27 @@
 import turtle as tr
 import module_briques as brick
 
+window = tr.Screen()
+window.bgcolor("black")
+window.setup(width=600, height=600)
+
 """Crée le message de victoire"""
 def message_victoire():
+    window.reset()
     message = tr.Turtle()
     message.speed(0)
-    message.color("withe")
-    message.hideturtle()
-    message.goto(0,0)
+    message.color("white")
+    message.penup()
+    message.setposition(0,0)
     message.write("Bravo tu as gagné !", align = "center",font=("chalkduster", 40, "normal"))
+    message.hideturtle()
     
-"""Détermine si il reste des briques"""
-def victoire():
-    if all(len(ligne) == 0 for ligne in brick.briques):
-        message_victoire()
+    
+
+
+    
+
+
+    
+        
         
