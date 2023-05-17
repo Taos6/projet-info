eeeb532eefd90fb9269c05fcc86e48a9585375bd
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-17 09:40:28 +0200

    changement vitesse balle

--- creation_mouvement_balle.py
+++ creation_mouvement_balle.py
@@ -10,8 +10,8 @@ ball.shapesize(stretch_wid = 0.75, stretch_len = 0.75)
 ball.color("red")
 ball.penup()
 ball.setposition(0,-150)
-ball.dx = -4
-ball.dy = -4
+ball.dx = -5
+ball.dy = -5
 
 
 def deplacement_balle():
