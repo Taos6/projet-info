900534d29f7688f83de25faf440e5af00528f1fd
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-17 02:09:19 +0200

    importation fichier collision_brique_balle

--- casse_brique_principal.py
+++ casse_brique_principal.py
@@ -5,7 +5,7 @@ import creation_mouvement_barre as move_barre
 import creation_coeur as coeur
 import collision_balle_barre as collision1
 import module_briques as brik
-
+import collision_brique_balle as collision2
 
 window = tr.Screen()
 window.bgcolor("black")
@@ -13,10 +13,12 @@ window.setup(width=600, height=600)
 
 coeur.nb_vie()
 
+
 while True:
     window.update()
     move_balle.deplacement_balle()
     collision1.collision_barre()
+    collision2.collision_brique()
     if move_balle.ball.ycor() < -280:
         print(move_balle.ball.ycor())
         coeur.vie -= 1
