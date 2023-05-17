cba0a3f30c39c6ccb6adff787484bbffb31d1e14
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-17 03:29:01 +0200

    suppression de certain critères

--- collision_brique_balle.py
+++ collision_brique_balle.py
@@ -9,9 +9,7 @@ import partie_gagnée as win
 def collision_brique():
     for ligne_briques in brick.briques:
         for brique in ligne_briques:
-            if brique.distance(move_balle.ball) < 25\
-            and move_balle.ball.ycor() < brique.ycor() + 25\
-            and move_balle.ball.ycor() > brique.ycor() - 25:
+            if brique.distance(move_balle.ball) < 25:
                 brique.hideturtle()
                 ligne_briques.remove(brique)
                 move_balle.ball.dy *= -1
