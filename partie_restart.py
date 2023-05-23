20fbc53450ad3f5b347cfd637223c0a61bd05dee
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-23 22:17:29 +0200

    suppression parte restart

--- partie_restart.py
+++
@@ -1,7 +0,0 @@
-
-
-def recommencer():
-    """ cette fonction permet à l'utilisateur de choisir s'il veut recommencer une partie ou non."""
-
-    rejouer = input("Veux-tu relancer une partie ?")
-    return rejouer.lower() == "oui"
