e433e27ccc3a0af6c2b5315cee2dbee59880f420
Taos6 <129945334+Taos6@users.noreply.github.com>
2023-05-23 21:48:29 +0200

    on enlève la ligne qui fais changer de direction la balle quand elle touche le bord du bas

--- casse_brique_principal.py
+++ casse_brique_principal.py
@@ -31,7 +31,6 @@ while True:
     if move_balle.ball.ycor() < -280:
         coeur.vie -= 1
         move_balle.ball.goto(0,-150)
-        move_balle.ball.dy *= -1
     elif coeur.vie == 2:
         coeur.vie3.hideturtle()
     elif coeur.vie == 1:
