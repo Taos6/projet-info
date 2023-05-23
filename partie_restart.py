ac26605c7aa56b2e9e8c0511135d376aed472dd5
Lagrove <131865400+Lagrove@users.noreply.github.com>
2023-05-23 18:25:40 +0200

    modification pas utile module restart

--- partie_restart.py
+++ partie_restart.py
@@ -1,5 +1,7 @@
 
+
 def restart():
-    
+    """ cette fonction permet à l'utilisateur de choisir s'il veut recommencer une partie ou non."""
+
     rejouer = input("Veux-tu relancer une partie ?")
     return rejouer.lower() == "oui"
