08d558d38d798ad1461790a4bd3a0c6f1c6876ca
maas165 <129945354+maas165@users.noreply.github.com>
2023-05-23 22:07:28 +0200

    Update creation_mouvement_balle.py

--- creation_mouvement_balle.py
+++ creation_mouvement_balle.py
@@ -7,8 +7,8 @@ tr.register_shape("balle.gif")
 ball = tr.Turtle()
 ball.speed(0)
 ball.shape("balle.gif")
-ball.shapesize(stretch_wid = 0.75, stretch_len = 0.75)
-ball.color("red")
+#ball.shapesize(stretch_wid = 0.75, stretch_len = 0.75)
+#ball.color("red")
 ball.penup()
 ball.setposition(0,-150)
 ball.dx = -3
@@ -19,7 +19,7 @@ def deplacement_balle():
     """Fais se déplacer la balle"""
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
-    if ball.xcor() > 285:  # Rebond à droite
+    if ball.xcor() > 285:     # Rebond à droite
         ball.dx *= -1
     elif ball.xcor() < -285:  # Rebond à gauche
         ball.dx *= -1
