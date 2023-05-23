229fbebcab02e652d37e86faf651bf8fa27558d1
Lagrove <131865400+Lagrove@users.noreply.github.com>
2023-05-23 19:24:49 +0200

    changement du nom en français

--- partie_restart.py
+++ partie_restart.py
@@ -1,6 +1,6 @@
 
 
-def restart():
+def recommencer():
     """ cette fonction permet à l'utilisateur de choisir s'il veut recommencer une partie ou non."""
 
     rejouer = input("Veux-tu relancer une partie ?")
