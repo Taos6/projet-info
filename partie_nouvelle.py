cd3667e3cca42111209301fcd238ab034c9bc75e
Lagrove <131865400+Lagrove@users.noreply.github.com>
2023-05-23 19:15:33 +0200

    mise à jour des indent
    le bout de code While True vient du code casse_brique_principal. Il est copié collé et il était nécéssaire de remettre les indentations à niveau

--- partie_nouvelle.py
+++ partie_nouvelle.py
@@ -3,19 +3,23 @@ import partie_restart
 def en_jeux():
     
     while True:
-    window.update()
-    move_balle.deplacement_balle()
-    collision1.collision_barre()
+        window.update()
+        move_balle.deplacement_balle()
+        collision1.collision_barre()
     
-    if move_balle.ball.ycor() < -280:
-        coeur.vie -= 1
-        move_balle.ball.goto(0,-150)
-        move_balle.ball.dy *= -1
-    elif coeur.vie == 2:
-        coeur.vie3.hideturtle()
-    elif coeur.vie == 1:
-        coeur.vie2.hideturtle()
-    elif coeur.vie == 0:
-        coeur.vie1.hideturtle()
-        over.game_over()
+        if move_balle.ball.ycor() < -280:
+            coeur.vie -= 1
+            move_balle.ball.goto(0,-150)
+            move_balle.ball.dy *= -1
+        elif coeur.vie == 2:
+            coeur.vie3.hideturtle()
+        elif coeur.vie == 1:
+            coeur.vie2.hideturtle()
+        elif coeur.vie == 0:
+            coeur.vie1.hideturtle()
+            over.game_over()
+           
         break
+    
+def restart():
+    
